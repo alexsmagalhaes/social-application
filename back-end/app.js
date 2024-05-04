bf5b7e6456111e1d562a671888764ed55e185dfa
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2024-05-04 11:26:03 -0300

    wip(add base structure application)
    Add a base for the db connection. Prevent Cors errors in the same server.

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,8 @@
 require("dotenv").config()
 
 const express = require("express")
+const cors = require("cors")
+const path = require("path")
 
 const port = process.env.PORT
 
@@ -8,6 +10,15 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+//solve cors
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }))
+
+//upload directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+
+//db connection
+require("./config/db.js")
 
 //routers
 const router = require("./routers/router")
