1eb29838782cb372488f62fec5d5c404d55ca9e1
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2024-05-04 11:45:58 -0300

    wip(create a db connection)

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,1 +1,22 @@
 const mongoose = require("mongoose")
+
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+
+const conn = async () => {
+   try {
+
+      const dbConnect = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.okgmt6j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+
+      console.log("Connection completed")
+
+      return dbConnect
+
+   } catch (error) {
+      console.log(error)
+   }
+}
+
+conn()
+
+module.exports = conn
