fd0a4c0d0a74679696462fdb3ddac2fb0f53878e
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2024-05-04 11:17:07 -0300

    feat(add routers and env variables)

--- back-end/app.js
+++ back-end/app.js
@@ -1,11 +1,18 @@
+require("dotenv").config()
+
 const express = require("express")
 
-const port = 500.
+const port = process.env.PORT
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+//routers
+const router = require("./routers/router")
+
+app.use(router)
 
 app.listen(port, () => {
    console.log(`App rodando na porta ${port}`)
